4ca7a7517cdf19e8528a0db03510edf2b2ec695c
sbaird22 <shanebaird30@yahoo.com>
2024-11-25 18:12:13 -0700

    added nav code

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,15 +1,12 @@
-import { Link, NavLink } from 'react-router-dom';
+import React from 'react';
+import Navigation from './Navigation';
 
 const Header = () => (
 <header>
-    <h1>Shane Baird </h1>
-    <nav>
-        <NavLink to="/" activeClassName="active">About Me</NavLink>
-        <NavLink to="/portfolio" activeClassName="active">Portfolio</NavLink>
-        <NavLink to="/contact" activeClassName="active">Contact</NavLink>
-        <NavLink to="/resume" activeClassName="active">Resume</NavLink>
-    </nav>
+    <h1>Shane Baird</h1>
+    <Navigation />
 </header>
 );
+
 export default Header;
 
