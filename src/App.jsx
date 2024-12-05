de171531ecaced9635ae73eead186bdfe3018dfc
sbaird22 <shanebaird30@yahoo.com>
2024-12-04 21:19:44 -0700

    bugs

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Footer from './components/Footer';
 import About from './pages/About';
 import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
-import Resume from './pages/Resume';
+import Resume from './components/resume';
 
 
 const App = () => (
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-
 const About = () => (
 <section>
     <h2>About Me</h2>
-    <img src="/path-to-your-photo.jpg" alt="Developer" />
+    <img src="/public/profile.png" alt="Developer" />
     <p>Hi, I'm Shane Baird, an aspiring developer currently honing my skills in a comprehensive coding bootcamp. With a foundation in modern web technologies and a passion for problem-solving, I'm eager to apply my growing expertise to real-world projects. I'm actively seeking opportunities to kickstart my career in coding and contribute to innovative teams.</p>
 </section>
 );
