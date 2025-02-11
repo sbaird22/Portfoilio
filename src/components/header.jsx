63f33b462c9bc6cffdc67664d1068c11ec85cea7
sbaird22 <shanebaird30@yahoo.com>
2025-02-11 10:48:07 -0700

    added logo and css fixes

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,6 +3,7 @@ import Navigation from './navigation';
 
 const Header = () => (
 <header>
+<img src="/assets/images/logo.png" alt="Shane Baird Logo" className="logo" />
     <h1>Shane Baird</h1>
     <Navigation />
 </header>
--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -3,12 +3,34 @@ import Project from '../components/project';
 
 const Portfolio = () => {
 const projects = [
-    { title: 'Weather App', repoLink: 'https://github.com/sbaird22/weather-app' },
-    { title: 'Food tracker app', repoLink: 'https://github.com/dmiddleton92/diet-food-tracker-app'},
-    { title: 'Company Table Sql', repoLink: 'https://github.com/sbaird22/company-tables' },
-    { title: 'Automobile Selector app', repoLink: 'https://github.com/sbaird22/auto-selector' },
-    { title: 'A README.md generator', repoLink: 'https://github.com/sbaird22/README-generator' },
-    { title: 'Portfolio', repoLink: 'https://github.com/sbaird22/Portfoilio' },
+    { 
+        title: 'Weather App',
+        image: '/assets/images/weather.png',
+        repoLink: 'https://github.com/sbaird22/weather-app'
+    },
+    { 
+        title: 'Food tracker app',
+        image: '/assets/images/diet.png',
+        repoLink: 'https://github.com/dmiddleton92/diet-food-tracker-app'
+    },
+    { 
+        title: 'Company Table Sql',
+        image: '/assets/images/company.png',
+        repoLink: 'https://github.com/sbaird22/company-tables' 
+    },
+    { 
+        title: 'Automobile Selector app',
+        image: '/assets/images/autoapp.png',
+        repoLink: 'https://github.com/sbaird22/auto-selector' 
+    },
+    { 
+        title: 'A README.md generator',
+        image: '/assets/images/readme.png',
+        repoLink: 'https://github.com/sbaird22/README-generator' },
+    { 
+        title: 'Job Tracker',
+        image: '/assets/images/job.png',
+        repoLink: 'https://github.com/sbaird22/job-application' },
     // Add more projects here
 ];
 
